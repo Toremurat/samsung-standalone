308f472176c2f96d2b1abf7a3ce43afe88229ac1
Toremurat <tawr.94@gmail.com>
2023-12-15 13:26:16 +0600

    global var Version fix

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ import "./static/styles/main.scss";
 const app = createApp(App);
 app.use(vueAxios, axios);
 app.use(router);
-app.mount("#app");
 axios
 	.get("/version")
 	.then((response) => {
 		const version = '?v=' + response.data.v;
 		app.config.globalProperties.$v = version;
+		app.mount('#app');
 	})
 	.catch((error) => {
 		console.error("Ошибка при получении Версии:", error);
